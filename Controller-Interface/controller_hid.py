b3ec286518c58e330ba8300ec2e86fee2b39ffd1
Robert Mones <robertmones42@gmail.com>
2024-11-01 18:29:20 -0400

    Fixed Update for 2024
    This reverts part of commit b41f08040d1129fef7a20f06438e1e1cbf41e864.

--- Controller-Interface/controller_hid.py
+++ Controller-Interface/controller_hid.py
@@ -16,10 +16,8 @@ def find_serial_port():
     elif sys.platform.startswith('win'):
         ports = filter(lambda x: x.pid == 29987, serial.tools.list_ports.comports())
         ports = list(map(lambda x: x.name, ports))
-    elif sys.platform.startswith('linux'):
-    	ports = ["/dev/ttyUSB0"]
     else:
-        raise Exception('UNKNOWN OPERATING SYSTEMS')
+        raise Exception('TODO:')
 
     radio = None
 
@@ -37,9 +35,17 @@ def find_serial_port():
     return radio
 
 def find_joystick():
-    joystick_id = (0x046d, 0xc21d)
+    joystick_id = None
+    for device in hid.enumerate():
+        if device['product_string'] == 'Logitech Dual Action':
+            joystick_id = (device["vendor_id"], device["product_id"])
+            break
+    if joystick_id is None:
+        print('Could not find controller! Is the controller plugged in?')
+        sys.exit(1)
+
     print("Found controller with id", joystick_id)
-    
+        
     joystick = hid.device()
     joystick.open(joystick_id[0], joystick_id[1])
     joystick.set_nonblocking(True)
@@ -62,7 +68,7 @@ def main():
             right_y = 255 - right_y
 
         t = time.time()
-        if t - last_t >= 0.03:
+        if t - last_t >= 0.1:
             joystick_state = [left_x, left_y, right_x, right_y, buttons1, buttons2]
 
             last_t = t
